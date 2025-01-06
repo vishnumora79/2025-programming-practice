26fd0d8c5ace946965a693e527ef4b4b4f38d7c2
vishnu mora <moravishnu2005@gmail.com>
2025-01-06 20:51:01 +0530

    Completed javascript crash course on codeevolution youtube channel for revising JS fundamental concepts

---
+++ JS/main.js
@@ -0,0 +1,192 @@
+console.log("Hello, from vishnu");
+
+// // Variable declaration: let and const
+// let age = 3;
+// console.log(age);
+
+// age = 7;
+// console.log(age);
+
+// const salary = 5;
+// console.log(salary);
+
+// photo = 7;
+// console.log(photo);
+// console.log(typeof photo);
+
+// // JS data types
+// // string
+// const name = "Vishnu";
+// const language = "JS";
+// const channel = "Codevolution";
+
+// // numbers
+// const total = 0;
+// const PI = 3.14;
+
+// // boolean
+// const isPrimeNumber = true;
+// const isNewUser = false;
+
+// // undefined
+// let result;
+// console.log(result)
+// const res = undefined;
+// console.log(res)
+// console.log(typeof undefined)
+
+// // null
+// const value = null;
+// console.log(value);
+// console.log(typeof null);
+
+// Objects
+
+// // object literal
+// const person = {
+//   firstName: "Tony",
+//   lastName: "Stark",
+//   age: 25,
+// };
+
+// console.log(person.firstName);
+
+// // Arrays
+// const oddNumbers = [1, 3, 5, 7];
+// console.log(oddNumbers[2]);
+
+// // Operators
+// // Arithematic operators
+// let x = 10;
+// let y = 5;
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+
+// ++x;
+// --y;
+
+// console.log(x);
+// console.log(y);
+
+// // Comparision operators
+const x = 5;
+const y = "5";
+// console.log(x == y);
+// console.log(x === y);
+// console.log(x != y);
+// console.log(x !== y);
+// console.log(x < 8 && y > 7)
+
+// // string operators
+// console.log("Tony " + "Stark");
+
+// const isEven = 10 % 2 === 0 ? "Even" : "Odd";
+// console.log(isEven)
+
+// console.log(Number('5'));
+// console.log(parseInt('5'));
+
+// console.log(String(5));
+// console.log((500).toString());
+// console.log(Boolean(10));
+
+// console.log(null == undefined)
+// console.log(null === undefined)
+
+// // conditional statements
+// const num = 5;
+// if (num > 0) {
+// console.log("positive");
+// } else  {
+//     console.log("negitive");
+// }
+
+// const num = 5;
+// if (num > 0) {
+// console.log("positive");
+// } else if (num < 0) {
+//     console.log("negitive");
+// } else {
+//     console.log("zero");
+// }
+
+// const color = "blue";
+// switch (color) {
+//     case 'red':
+//         console.log("color is red");
+//         break;
+//     case 'blue':
+//         console.log("color is blue");
+//         break;
+//     default:
+//         console.log("no color matched!");
+//         break;
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log("Iteration number " + i);
+// }
+
+// let i = 1;
+
+// while (i <= 5) {
+//     console.log("Iteration number " + i);
+//     i++;
+// }
+
+// let i = 1;
+// do {
+//     console.log("Iteration number " + i);
+//     i++;
+// } while(i <= 5);
+
+// const numArray = [1, 2, 3, 5, 5];
+// for(const num of numArray) {
+//     console.log(num);
+// }
+
+// for(const num in numArray) {
+//     console.log(num);
+// }
+
+// function greet (name) {
+//     console.log(`Good morning ${name}`);
+// }
+
+// greet('Vishnu');
+// greet('Anju');
+// greet('Ammu');
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// const res = add(2, 3);
+// console.log(res);
+
+// const arraySum = (a, b) => {
+//   return a + b;
+// };
+
+// const sum = arraySum(25, 25);
+// console.log(sum);
+
+const num = 100;
+if (true) {
+  const myName = "Vishnu";
+  console.log(myName);
+  console.log(num);
+}
+
+function testFn() {
+  const myName = "Iron Man";
+  console.log(myName);
+  console.log(num);
+}
+
+testFn();
+
+console.log(num);
+
